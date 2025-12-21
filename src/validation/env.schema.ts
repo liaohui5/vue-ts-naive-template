36c628c201a9932048acd6f18780a0516aca4cf1
secret <secretx500@qq.com>
2025-12-22 01:01:43 +0800

    :bug: fix: Standardize VITE_APP_API_BASE_URL default and VITE_APP_USE_MSW parsing

--- src/validation/env.schema.ts
+++ src/validation/env.schema.ts
@@ -9,8 +9,8 @@ export const envZod = z.object({
   BASE_URL: z.string(),
 
   // 接口请求地址
-  VITE_APP_API_BASE_URL: z.string().default("/"),
+  VITE_APP_API_BASE_URL: z.string().default(""),
 
   // 是否启用 mock service worker
-  VITE_APP_USE_MSW: z.enum(["false", "true"]).refine((v) => JSON.parse(v)),
+  VITE_APP_USE_MSW: z.enum(["false", "true"]).overwrite((v) => JSON.parse(v)),
 });
